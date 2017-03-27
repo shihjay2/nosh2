5939b6ffd51b0e87283d291027fb1ec12230c059
Michael Shihjay Chen <shihjay2@gmail.com>
2017-03-26 18:27:31 -0700

    Show accept invitation link for unregistered users for installs wihtout domain name.

--- app/Http/Controllers/CoreController.php
+++ app/Http/Controllers/CoreController.php
@@ -7380,6 +7380,12 @@ class CoreController extends Controller
             foreach ($result as $row) {
                 $arr = [];
                 $arr['label'] = '<b>' . $row->displayname . '</b> - ' . $row->username;
+                $google = File::get(base_path() . "/.google");
+                if ($google == '') {
+                    if ($row->secret_question == null) {
+                        $arr['label'] .= '<br><a href="' . route('accept_invitation', [$row->password]) . '" target="_blank">' . route('accept_invitation', [$row->password]) . '</a>';
+                    }
+                }
                 $arr['edit'] = route('core_form', ['users', $row_index, $row->$row_index, $type]);
                 if ($active == '1') {
                     $arr['inactivate'] = route('core_action', ['table' => 'users', 'action' => 'inactivate', 'index' => $row_index, 'id' => $row->$row_index]);
