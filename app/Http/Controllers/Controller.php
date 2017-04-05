52a03384059d382242ee83be069ffa6dbbed5685
Michael Shihjay Chen <shihjay2@gmail.com>
2017-04-05 10:31:21 -0700

    Fixed format recognition for issues in CCDA generation.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -445,167 +445,6 @@ class Controller extends BaseController
         return $ethnicity;
     }
 
-    protected function array_fax()
-    {
-        $return = [
-            'us' => [
-                '@email.uscc.net',
-                '@message.alltel.com',
-                '@messaging.sprintpcs.com',
-                '@mobile.celloneusa.com',
-                '@msg.telus.com',
-                '@paging.acswireless.com',
-                '@pcs.rogers.com',
-                '@qwestmp.com',
-                '@sms.mycricket.com',
-                '@sms.ntwls.net',
-                '@tmomail.net',
-                '@txt.att.net',
-                '@txt.windmobile.ca',
-                '@vtext.com',
-                '@text.republicwireless.com',
-                '@msg.fi.google.com'
-                //'@message.ting.com', // Duplicate of Sprint
-                //'@sms.edgewireless.com',  // slow
-            ],
-            'canada' => [
-                '@blueskyfrog.com',
-                '@bplmobile.com',
-                '@cellularonewest.com',
-                '@clearlydigital.com',
-                '@comcastpcs.textmsg.com',
-                '@corrwireless.net',
-                '@csouth1.com',
-                '@cwemail.com',
-                '@cwwsms.com',
-                '@email.swbw.com',
-                '@email.uscc.net',
-                '@fido.ca',
-                '@ideacellular.net',
-                '@inlandlink.com',
-                '@ivctext.com',
-                '@message.alltel.com',
-                '@messaging.centurytel.net',
-                '@messaging.sprintpcs.com',
-                '@mobile.celloneusa.com',
-                '@mobile.dobson.net',
-                '@mobile.surewest.com',
-                '@mobilecomm.net',
-                '@msg.clearnet.com',
-                '@msg.koodomobile.com',
-                '@msg.telus.com',
-                '@my2way.com',
-                '@myboostmobile.com',
-                '@onlinebeep.net',
-                '@page.metrocall.com',
-                '@pagemci.com',
-                '@paging.acswireless.com',
-                '@pcs.rogers.com',
-                '@pcsone.net',
-                '@qwestmp.com',
-                '@satellink.net',
-                '@sms.3rivers.net',
-                '@sms.bluecell.com',
-                '@sms.edgewireless.com',
-                '@sms.goldentele.com',
-                '@sms.pscel.com',
-                '@sms.wcc.net',
-                '@text.houstoncellular.net',
-                '@text.mtsmobility.com',
-                '@tmomail.net',
-                '@tms.suncom.com',
-                '@txt.att.net',
-                '@txt.bell.ca',
-                '@txt.northerntelmobility.com',
-                '@txt.windmobile.ca',
-                '@uswestdatamail.com',
-                '@utext.com',
-                '@vmobile.ca',
-                '@vmobl.com',
-                '@vtext.com',
-              ],
-            'intl' => [
-                '@airtelchennai.com',
-                '@airtelkol.com',
-                '@airtelmail.com',
-                '@alphame.com',
-                '@bluewin.ch',
-                '@bplmobile.com',
-                '@c.vodafone.ne.jp',
-                '@celforce.com',
-                '@correo.movistar.net',
-                '@delhi.hutch.co.in',
-                '@digitextjm.com',
-                '@e-page.net',
-                '@escotelmobile.com',
-                '@freesurf.ch',
-                '@gsm1800.telia.dk',
-                '@h.vodafone.ne.jp',
-                '@ideacellular.net',
-                '@itelcel.com',
-                '@m1.com.sg',
-                '@ml.bm',
-                '@mmail.co.uk',
-                '@mobilpost.no',
-                '@mobistar.be',
-                '@mobtel.co.yu',
-                '@movistar.net',
-                '@msgnextel.com.mx',
-                '@msg.globalstarusa.com',
-                '@msg.iridium.com',
-                '@mujoskar.cz',
-                '@mymeteor.ie',
-                '@mysmart.mymobile.ph',
-                '@mysunrise.ch',
-                '@o2.co.uk',
-                '@o2imail.co.uk',
-                '@onemail.at',
-                '@onlinebeep.net',
-                '@optusmobile.com.au',
-                //'@orange.net',
-                //'@orangemail.co.in',
-                '@page.mobilfone.com',
-                '@page.southernlinc.com',
-                '@pageme.teletouch.com',
-                '@pager.irkutsk.ru',
-                '@pcs.ntelos.com',
-                '@rek2.com.mx',
-                '@rpgmail.net',
-                '@safaricomsms.com',
-                '@satelindogsm.com',
-                '@scs-900.ru',
-                '@sfr.fr',
-                '@sms.co.tz',
-                '@sms.comviq.se',
-                '@sms.emt.ee',
-                '@sms.goldentele.com',
-                '@sms.luxgsm.lu',
-                '@sms.netcom.no',
-                //'@sms.orange.nl',
-                '@sms.primtel.ru',
-                '@sms.t-mobile.at',
-                '@sms.tele2.lv',
-                '@sms.umc.com.ua',
-                '@sms.uraltel.ru',
-                '@sms.vodafone.it',
-                '@smsmail.lmt.lv',
-                '@swmsg.com',
-                '@t-d1-sms.de',
-                '@t-mobile-sms.de',
-                '@t-mobile.uk.net',
-                '@t.vodafone.ne.jp',
-                '@text.mtsmobility.com',
-                '@text.simplefreedom.net',
-                '@timnet.com',
-                '@vodafone.net',
-                '@wyndtell.com',
-                //'sms@pcom.ru (put the number in the subject line)',
-                //'pinnumber@jsmtel.com',
-            ]
-        ];
-        return $return;
-    }
-
     protected function array_gender()
     {
         $gender = [
@@ -9122,8 +8961,11 @@ class Controller extends BaseController
             $l = 1;
             foreach ($issues_query as $issues_row) {
                 $issues_table .= "<list listType='ordered'>";
+                $issue_code = '';
                 $issues_array = explode(' [', $issues_row->issue);
-                $issue_code = str_replace("]", "", $issues_array[1]);
+                if (count($issues_array) > 1) {
+                    $issue_code = str_replace("]", "", $issues_array[1]);
+                }
                 $issue_code_description = $issues_array[0];
                 if ($issues_row->issue_date_inactive != '0000-00-00 00:00:00') {
                     $issues_table .= "<item><content ID='problem" . $l . "'>" . $issues_row->issue . ": Status - Resolved</content></item>";
@@ -14931,7 +14773,7 @@ class Controller extends BaseController
         return 'Fax Job ' . $job_id . ' Sent';
     }
 
-    protected function send_mail($template, $data_message, $subject, $to, $practice_id, $sms=false)
+    protected function send_mail($template, $data_message, $subject, $to, $practice_id)
     {
         $practice = DB::table('practiceinfo')->where('practice_id', '=', $practice_id)->first();
         $file = File::get(base_path() . "/.google");
@@ -14966,30 +14808,12 @@ class Controller extends BaseController
                 $transport->setPassword($password);
             }
             Mail::setSwiftMailer(new Swift_Mailer($transport));
-            Mail::send($template, $data_message, function ($message) use ($to, $subject, $practice, $sms) {
-                if ($sms == false) {
-                    $message->to($to)
-                        ->from($practice->email, $practice->practice_name)
-                        ->subject($subject);
-                } else {
-                    $message->to($to)
-                        ->from($practice->smtp_user . '@gmail.com')
-                        ->subject($subject);
-                }
+            Mail::send($template, $data_message, function ($message) use ($to, $subject, $practice) {
+                $message->to($to)
+                    ->from($practice->email, $practice->practice_name)
+                    ->subject($subject);
             });
         }
-        return true;
-    }
-
-    protected function send_sms($template, $data_message, $subject, $number, $practice_id, $type='us')
-    {
-        $number = preg_replace('/\D+/', '', $number);
-        $sms_arr = $this->array_fax();
-        $to = [];
-        foreach ($sms_arr[$type] as $sms) {
-            $to[] = $number . $sms;
-        }
-        $this->send_mail(['text' => $template], $data_message, $subject, $to, $practice_id, true);
         return true;
     }
 
