bcd99f8ad7eebf88c4197797dd16ce15b9e5debb
Michael Shihjay Chen <shihjay2@gmail.com>
2017-04-12 06:22:48 -0700

    Prevent session collision with HIE of One AS

--- config/session.php
+++ config/session.php
@@ -109,7 +109,7 @@ return [
     |
     */
 
-    'cookie' => 'laravel_session_uma',
+    'cookie' => 'laravel_session_nosh',
 
     /*
     |--------------------------------------------------------------------------
