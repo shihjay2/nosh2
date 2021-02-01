00bd206bb24353a7e3dcdee06cedfe0f8690b14d
Michael Shihjay Chen <shihjay2@gmail.com>
2021-01-31 16:53:29 -0800

    Fix issue #168

--- config/logging.php
+++ config/logging.php
@@ -45,7 +45,7 @@ return [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
-            'permission' => 0664,
+            'permission' => 0666,
         ],
 
         'daily' => [
@@ -53,7 +53,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
-            'permission' => 0664,
+            'permission' => 0666,
         ],
 
         'slack' => [
