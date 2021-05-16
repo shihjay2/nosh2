96a57ded4c191eb9086af782fc5822e21a04232f
Dan Heslinga <dan@heslinga.us>
2021-05-15 16:30:18 -1000

    prevent key encryption jquery.fileDownload.js so modal disappear as expected after download

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -17,7 +17,7 @@ class EncryptCookies extends BaseEncrypter
         // For jquery.fileDownload.js to work properly you need to return a cookie fileDownload=true.
         // You don't want 'true' encrypted. 
         // Without this the the cookie is fileDownload=a_very_long_jumble_of_characters
-        // and the fileDonwload successCallback will not trigger 
+        // and the fileDownload successCallback will not trigger 
         // leaving your modal open after the download is complete. 
         'fileDownload',
     ];
